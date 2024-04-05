77eb9823734bb05e3ed59c765c999e8b81105194
KIT-DEV-L12\Rezza <rezza.wahid@kst.co.id>
2024-04-05 19:19:05 +0700

    up img

--- src/js/data/2023-07-16.js
+++ src/js/data/2023-07-16.js
@@ -16,161 +16,161 @@ dataSet[dataSetVersion].options = [
 dataSet[dataSetVersion].characterData = [
   {
     name: "Akita Rian",
-    img: "akita_rian.jpeg",
+    img: "akita_rian.jpg",
     opts: {
       gen: [...genList],
     }
   },
   {
     name: "Anno Aoi",
-    img: "anno_aoi.jpeg",
+    img: "anno_aoi.jpg",
     opts: {
       gen: [...genList],
     }
   },
   {
     name: "Aoki Yuho",
-    img: "aoki_yuho.jpeg",
+    img: "aoki_yuho.jpg",
     opts: {
       gen: [...genList],
     }
   },
   {
     name: "Hagiwara Kokoka",
-    img: "hagiwara_kokoka.jpeg",
+    img: "hagiwara_kokoka.jpg",
     opts: {
       gen: [...genList],
     }
   },
   {
     name: "Hasegawa Hitomi",
-    img: "hasegawa_hitomi.jpeg",
+    img: "hasegawa_hitomi.jpg",
     opts: {
       gen: [...genList],
     }
   },
   {
     name: "Hayasaki Suzuki",
-    img: "hayasaki_suzuki.jpeg",
+    img: "hayasaki_suzuki.jpg",
     opts: {
       gen: [...genList],
     }
   },
   {
     name: "Imai Yuki",
-    img: "imai_yuki.jpeg",
+    img: "imai_yuki.jpg",
     opts: {
       gen: [...genList],
     }
   },
   {
     name: "Ito Yuzu",
-    img: "ito_yuzu.jpeg",
+    img: "ito_yuzu.jpg",
     opts: {
       gen: [...genList],
     }
   },
   {
     name: "Iwamoto Riko",
-    img: "iwamoto_riko.jpeg",
+    img: "iwamoto_riko.jpg",
     opts: {
       gen: [...genList],
     }
   },
   {
     name: "Kanazawa Ami",
-    img: "kanazawa_ami.jpeg",
+    img: "kanazawa_ami.jpg",
     opts: {
       gen: [...genList],
     }
   },
   {
     name: "Kinoshita Ai",
-    img: "kinoshita_ai.jpeg",
+    img: "kinoshita_ai.jpg",
     opts: {
       gen: [...genList],
     }
   },
   {
     name: "Kudo Yua",
-    img: "kudo_yua.jpeg",
+    img: "kudo_yua.jpg",
     opts: {
       gen: [...genList],
     }
   },
   {
     name: "Miyakoshi Yuria",
-    img: "miyakoshi_yuria.jpeg",
+    img: "miyakoshi_yuria.jpg",
     opts: {
       gen: [...genList],
     }
   },
   {
     name: "Mochinaga Mana",
-    img: "mochinaga_mana.jpeg",
+    img: "mochinaga_mana.jpg",
     opts: {
       gen: [...genList],
     }
   },
   {
     name: "Nishimori Aya",
-    img: "nishimori_aya.jpeg",
+    img: "nishimori_aya.jpg",
     opts: {
       gen: [...genList],
     }
   },
   {
     name: "Shiogama Nana",
-    img: "shiogama_nana.jpeg",
+    img: "shiogama_nana.jpg",
     opts: {
       gen: [...genList],
     }
   },
   {
     name: "Sugiura Eren",
-    img: "sugiura_eren.jpeg",
+    img: "sugiura_eren.jpg",
     opts: {
       gen: [...genList],
     }
   },
   {
     name: "Sunaga Miuna",
-    img: "sunaga_miuna.jpeg",
+    img: "sunaga_miuna.jpg",
     opts: {
       gen: [...genList],
     }
   },
   {
     name: "Yaegashi Miisa",
-    img: "yaegashi_miisa.jpeg",
+    img: "yaegashi_miisa.jpg",
     opts: {
       gen: [...genList],
     }
   },
   {
     name: "Yagi Toa",
-    img: "yagi_toa.jpeg",
+    img: "yagi_toa.jpg",
     opts: {
       gen: [...genList],
     }
   },
   {
     name: "Yamaguchi Yuan",
-    img: "yamaguchi_yuan.jpeg",
+    img: "yamaguchi_yuan.jpg",
     opts: {
       gen: [...genList],
     }
   },
   {
     name: "Yanagihori Karen",
-    img: "yanagihori_karen.jpeg",
+    img: "yanagihori_karen.jpg",
     opts: {
       gen: [...genList],
     }
   },
   {
     name: "Yoshimoto Cocona",
-    img: "yoshimoto_cocona.jpeg",
+    img: "yoshimoto_cocona.jpg",
     opts: {
       gen: [...genList],
     }

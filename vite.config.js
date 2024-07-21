5a56fb6a41bf9b40e06862ab399115192671ce91
KIT-DEV-L12\Rezza <rezza.wahid@kst.co.id>
2024-07-21 17:22:05 +0700

    add asset include

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  assetsInclude: ['/src/js/members/data.js', './src/js/members/html2canvas.min.js'],
   plugins: [react()],
   base: "/bokuao-sorter/"
 })
